10d08370c01fc9c05cc36147fe5bd4ef763316b5
Jérôme Jutteau <jerome.jutteau@outscale.com>
2024-01-17 17:04:24 +0100

    Fix default config path
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

--- src/args.rs
+++ src/args.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
 
+static DEFAULT_CONFIG_PATH: &str = "/etc/osc/bsud.json";
+
 pub fn parse() -> Args {
     Args::parse()
 }
@@ -7,6 +9,6 @@ pub fn parse() -> Args {
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about=None)]
 pub struct Args {
-    #[arg(long = "config", short = 'c', default_value_t = String::from("/etc/osc/bsud/config.json"))]
+    #[arg(long = "config", short = 'c', default_value_t = String::from(DEFAULT_CONFIG_PATH))]
     pub config_path: String,
 }
